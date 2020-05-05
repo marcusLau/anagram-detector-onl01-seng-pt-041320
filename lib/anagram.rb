6005b7610cb5756353f8c166a75ce074cbc78caa
Marcus Lau <lau.marcus76@gmail.com>
2020-05-05 19:08:18 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,6 +8,10 @@ class Anagram
   end
   
   def match(arr)
-    
+    # an array of words 
+    # per each element split AND sort the word to get a sorted letter array
+      # check if that sorted letter array matches @anagram.split.sorted 
+      # if sorted add to anagram_result_arr to return
+    # if no anagrams found 
   end
 end
